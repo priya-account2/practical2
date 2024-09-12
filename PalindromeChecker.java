c14d81e1d1bb659bb12ea122b1327c5ed6bff21e
Priyadharshini <priyadhashini47@gmail.com>
2024-09-12 14:34:27 +0530

    file added successfully

---
+++ PalindromeChecker.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner("radar");
+        String str = scanner.nextLine();
+        if (isPalindrome(str)) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+        scanner.close();
+    }
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
